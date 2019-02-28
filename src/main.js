a1672fbc8a96532d7b175b68d75f25f894cd05a8
jeferwang <1737723363@qq.com>
2019-03-01 00:35:47 +0800

    install element

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import i18n from './i18n'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
